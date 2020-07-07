535c89cf8c9312cf9faed10c703d1b5dc28e2204
Gabriele Etta <etta.gabriele@gmail.com>
2020-07-07 19:58:56 +0200

    Qui sotto c'è uno con il raggaeton e voglio pushare per scappare

--- src/Classification/Gender_User_Classification.R
+++ src/Classification/Gender_User_Classification.R
@@ -1,4 +1,5 @@
 # Dataset available at: https://archive.ics.uci.edu/ml/datasets/adult
+rm(list = ls())
 
 # Libraries
 library(tibble)
@@ -15,6 +16,8 @@ library(pals)
 library(glmnet)
 library(pRoc)
 library(caret)
+library(leaps)
+library(fastDummies)
 
 # Global Variables ####
 dir = "C:\\Users\\ettag\\Documents\\GitHub\\Stastistical-Learning-Project\\data"
@@ -32,7 +35,7 @@ drug.cols = c("has_taken_illegal_drugs", "has_taken_drugs", "has_taken_synthetic
 drugs.clean = drugs.clean %>% select(-drug.cols)
 
 # Divide dataset in train and test set ####
-train.rows = round(dim(drugs.clean)[1] * 0.60)
+train.rows = round(dim(drugs.clean)[1] * 0.70)
 test.rows = dim(drugs.clean)[1] - train.rows
 
 set.seed(42)
@@ -41,56 +44,115 @@ train = sample(1:nrow(drugs.clean), train.rows)
 train.ds = drugs.clean[train, ]
 test.ds = drugs.clean[-train,]
 
-# Build model with LASSO and 5 Fold CV
-lambda_seq <- 10^seq(2, -2, by = -.1)
-grid = grid=10^seq(10,-2,length=100)
+# Performing best subset selection
+variables = 12
+best.subset.selection = regsubsets(Gender ~., train.ds, really.big = T, nvmax = variables)
+best.subset.summary = summary(best.subset.selection)
 
-# glmnet wants a matrix, so we prepare data accordingly.
-x = model.matrix(Gender~., train.ds)
-y = ifelse(train.ds$Gender == "Female",1,0)
-cv_output <- cv.glmnet(x,y,
-                       alpha = 1,
-                       nfolds = 5, family = "binomial")
+# Plot RSS, adjusted r-square, Cp, BIC for all the models at once
+par(mfrow = c(2, 2))
 
-plot(cv_output)
+# RSS Plot
+plot(best.subset.summary$rss, xlab = "Number of Variables", ylab = "RSS", type = "l")
 
-# Looking for the best lambda.
-best_lam = cv_output$lambda.min 
-lambda_1se = cv_output$lambda.1se
+# Adjusted RSq plot
+plot(best.subset.summary$adjr2, xlab = "Number of Variables", ylab = "Adjusted RSq", 
+     type = "l")
+which.max(best.subset.summary$adjr2) # 11
+points(11, best.subset.summary$adjr2[11], col = "red", cex = 2, pch = 20)
 
-# Which coefficients we obtained?
-coef(cv_output,s=lambda_1se)
+# Cp
+plot(best.subset.summary$cp, xlab = "Number of Variables", ylab = "Cp", type = "l")
+which.min(best.subset.summary$cp) # 11
+points(11, best.subset.summary$cp[11], col = "red", cex = 2, pch = 20)
 
-# Test
-x_test = model.matrix(Gender ~.,test.ds)
-lasso_prob = predict(cv_output, newx = x_test, s=lambda_1se, type= "response")
+# BIC
+plot(best.subset.summary$bic, xlab = "Number of Variables", ylab = "BIC", type = "l")
+which.min(best.subset.summary$bic) # 10
+points(10, best.subset.summary$bic[10], col = "red", cex = 2, pch = 20)
 
-lasso_predict = rep("Male",nrow(test.ds))
-lasso_predict[lasso_prob>.5] <- "Female"
+# Plotting regsubsets result
+par(mfrow = c(2,2))
+# Plotting built into regsubsets()
+plot(best.subset.selection, scale = "r2")
+plot(best.subset.selection, scale = "adjr2")
+plot(best.subset.selection, scale = "Cp")
+plot(best.subset.selection, scale = "bic")
+
+par(mfrow = c(1,1))
+
+# Choosing the model with the lowest BIC
+coef(best.subset.selection, 10)
+
+# Extending the dataset by unfolding its dummy in order to choose the
+# specific categories found from best subset selection model.
+train.ds = fastDummies::dummy_cols(train.ds)
+
+# Checking if specificity and sensivity holds through K fold CV.
+
+# Randomly shuffle the data
+train.ds.shuffled = train.ds[sample(nrow(train.ds)),]
+
+#Create 10 equally size folds
+folds <- cut(seq(1,nrow(train.ds.shuffled)), breaks=10, labels=FALSE)
+
+#Perform 10 fold cross validation
+specificity.cv = rep(0, 10)
+sensitivity.cv = rep(0, 10)
+i = 1
+for(i in 1:10){
+  
+  testIndexes = which(folds==i,arr.ind=TRUE)
+  
+  testData = train.ds.shuffled[testIndexes, ]
+  
+  trainData = train.ds.shuffled[-testIndexes, ]
+  
+  model.train = glm(Gender ~ `Age_25-34` + Education_BS + Education_MS + Country_UK + Nscore + Ascore + Cscore + 
+                      Amyl_TRUE + Legalh_TRUE + LSD_TRUE, data = train.ds, family = "binomial")
+  predictions.cv = predict.glm(model.train, newdata = testData, type = "response")
+  
+  # Confusion matrix
+  predicted.categories = ifelse(predictions.cv > 0.5, "Male", "Female")
+  #classification.table = table(pred = predicted.categories, true = testData$Gender)
+  conf.matrix = confusionMatrix(table(predicted.categories, testData$Gender)
+  
+  specificity.cv[i] = specificity(conf.matrix)
+  sensitivity.cv[i] = sensitivity.cv(conf.matrix)
+}
+
+
+
+
+
+# Creating 10 folds per CV
+train.cv = trainControl(method = "repeatedcv", number = 10, repeats = 3)
+
+# Train the model
+train.model <- train(Gender ~ `Age_25-34` + Education_BS + Education_MS + Country_UK + Nscore + Ascore + Cscore + 
+                 Amyl_TRUE + Legalh_TRUE + LSD_TRUE, data = train.ds, method = "glm",
+               trControl = train.cv)
+
+# Extending for test dataset too.
+test.ds = fastDummies::dummy_cols(test.ds)
+predictions = predict.glm(train.model, newdata = test.ds, type = "response")
 
 # Confusion matrix
-classification.table = table(pred = lasso_predict, true = test.ds$Gender)
+predicted.categories = ifelse(predictions > 0.5, "Male", "Female")
+classification.table = table(pred = predicted.categories, true = test.ds$Gender)
 classification.table
 
 # Error rate
 (112 + 120) / (112 + 270 + 252 + 120)
-
-# confusionMatrix(classification.table)
-
-# Accuracy
-mean(lasso_predict==test.ds$Gender)
+confusionMatrix(classification.table)
 
 # Roc Curve
-numeric_predict = ifelse(lasso_predict == "Female",1,0)
-numeric_real = ifelse(test.ds$Gender == "Female",1,0)
+numeric_real = ifelse(test.ds$Gender == "Male",1,0)
 
-roc.out <- pROC::roc(numeric_predict, numeric_real, levels=c(0, 1))
-plot.roc = plot(roc.out,  
+roc.out <- pROC::roc(numeric_real~predictions)
+plot(roc.out,  
                 print.auc=TRUE, 
+                plot = TRUE,
                 main = "Gender Drug / Non Drug Users Classification",
-                legacy.axes=TRUE, 
                 xlab="False positive rate", ylab="True positive rate")
-plot.roc
 
-# Clean ####
-rm(list = ls())
